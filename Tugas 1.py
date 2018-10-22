8e79fe25a19d8e2d39893ade167f4d4fe2b2118c
Anugerah Nuri Satria <anugerahnurisatria@gmail.com>
2018-10-22 23:33:36 +0700

    Perubahan Data

--- Tugas 1.py
+++
@@ -1,7 +0,0 @@
-hargabuku = input("Masukkan harga terbaru buku: ")
-diskon = input ("Masukkan diskon toko: ")
-
-hargaawal=  int (hargabuku) * float (diskon)
-
-hargaakhir = int ((hargaawal) + 9000) + int(((hargaawal) + 3000) *59)   
-print('Harga untuk pengiriman 60 eksemplar adalah: ', hargaakhir)
